307dbaef5f52306d3c0af91b9fad8efe2dde796d
Billy Needham <billyluisneedham@gmail.com>
2020-01-10 10:13:25 +0000

    expanding on fuel-cal

--- fuel-calculator/src/containers/App.js
+++ fuel-calculator/src/containers/App.js
@@ -1,18 +1,33 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
 
-  const [remainingFuel, setRemainingFuel] = useState(0);
+  const [remainingFuel, setRemainingFuel] = useState('');
+  const [specificGravity, setSpecificGravity] = useState('');
+  const [finalFuelState, setFinalFuelState] = useState('');
+  const [deliveredFuel, setDeliveredFuel] = useState('');
 
   return (
     <div className="App">
       <h1>Enginnering Fuel Calculator</h1>
       <form>
-        Aircraft Remaining Fuel (Kg) <input type='text' /> <br />
-        Specific Gravity <input type='text' /><br />
-        Aircraft Final Fuel State (Kg) <input type='text' /><br />
-        Delivered Fuel (Lts) <input type='text' /><br />
+        Aircraft Remaining Fuel (Kg) <input
+          type='text'
+          value={remainingFuel}
+          onChange={e => setRemainingFuel(e.target.value)}
+        /> <br />
+        Specific Gravity <input 
+          type='text'
+          value={specificGravity}
+          onChange={e => setSpecificGravity(e.target.value)} /><br />
+        Aircraft Final Fuel State (Kg) <input 
+          type='text'
+          value={finalFuelState}
+          onChange={setFinalFuelState} /><br />
+        Delivered Fuel (Lts) <input 
+          type='text'
+          value={} /><br />
         <span id='disc-box'>Discrepancy <input type='text' placeholder='0%' /><br /></span>
       </form>
     </div>
