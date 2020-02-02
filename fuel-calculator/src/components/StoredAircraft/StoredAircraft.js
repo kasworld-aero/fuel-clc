f38ec5e2e909ea63166c3768cfd510f3cb584a00
Billy N <billyluisneedham@gmail.com>
2020-02-02 18:44:55 +0000

    Styled stored aircraft. WIP

--- fuel-calculator/src/components/StoredAircraft/StoredAircraft.js
+++ fuel-calculator/src/components/StoredAircraft/StoredAircraft.js
@@ -1,6 +1,7 @@
 import React from "react";
 import StoredAircraftLi from "./StoredAircraftLi";
 import Button from "@material-ui/core/Button";
+import './StoredAircraft.css';
 
 const StoredAircraftCont = props => {
   const storedAircraft = props.storedAircraft;
@@ -17,34 +18,37 @@ const StoredAircraftCont = props => {
 
   return (
     <div>
-      <h2>Stored Aircraft Details</h2>
-      <ul>
-        {storedAircraft.map((storedAircraft, index) => (
-          <StoredAircraftLi
-            key={index}
-            className="input-box"
-            storedAircraft={storedAircraft}
-          />
-        ))}
-      </ul>
-      <div className="button-cont">
-        <div className="button-wrapper">
-          <Button
-            variant="contained"
-            color="secondary"
-            onClick={confirmResetWindow}
-          >
-            Reset
-          </Button>
-          <Button
-            variant="contained"
-            color="primary"
-            onClick={props.setShowStoredAircraft}
-          >
-            Back
-          </Button>
-        </div>
+      {/* <div className="input-cont"> */}
+      <div>
+        <h1>Stored Aircraft Details</h1>
+        <ul>
+          {storedAircraft.map((storedAircraft, index) => (
+            <StoredAircraftLi
+              key={index}
+              className="input-box"
+              storedAircraft={storedAircraft}
+            />
+          ))}
+        </ul>
       </div>
+        <div className="button-cont">
+          <div className="button-wrapper">
+            <Button
+              variant="contained"
+              color="secondary"
+              onClick={confirmResetWindow}
+            >
+              Reset
+            </Button>
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={props.setShowStoredAircraft}
+            >
+              Back
+            </Button>
+          </div>
+        </div>
     </div>
   );
 };
--- fuel-calculator/src/components/StoredAircraft/StoredAircraftLi.js
+++ fuel-calculator/src/components/StoredAircraft/StoredAircraftLi.js
@@ -1,15 +1,17 @@
 import React, { useState } from "react";
+import Paper from '@material-ui/core/Paper';
 
 const StoredAircraftLi = props => {
   const [classEnabled, setClassEnabled] = useState(false);
 
   return (
     <li
-      onClick={() => setClassEnabled(!classEnabled)}
-      className="input-box"
-      style={classEnabled ? { textDecoration: "line-through" } : {}}
-    >
-      {props.storedAircraft}
+        onClick={() => setClassEnabled(!classEnabled)}
+        style={classEnabled ? { textDecoration: "line-through" } : {}}
+      >
+      <Paper className='input-box'>
+        {props.storedAircraft}
+      </Paper>
     </li>
   );
 };
