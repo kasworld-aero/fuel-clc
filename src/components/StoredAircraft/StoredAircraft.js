b6b84514f2731cd077604dfea34aad088c5058b8
Billy Needham <billyluisneedham@gmail.com>
2020-02-09 23:40:01 +0000

    Stored Aircraft LI refactored

--- src/components/StoredAircraft/StoredAircraft.js
+++ src/components/StoredAircraft/StoredAircraft.js
@@ -1,20 +1,19 @@
 import React from "react";
 import StoredAircraftLi from "./StoredAircraftLi";
 import Button from "@material-ui/core/Button";
-import Paper from '@material-ui/core/Paper';
+import Paper from "@material-ui/core/Paper";
 
 const StoredAircraftCont = props => {
   const storedAircraft = props.storedAircraft;
-  
 
-    // Object.keys(ingredients)
-    //   .map(igKey => {
-    //     return ingredients[igKey];
-    //   })
-    //   .reduce((sum, el) => {
-    //     return sum + el;
-    //   }, 0);
-    // this.setState({ purchasable: sum > 0 });
+  // Object.keys(ingredients)
+  //   .map(igKey => {
+  //     return ingredients[igKey];
+  //   })
+  //   .reduce((sum, el) => {
+  //     return sum + el;
+  //   }, 0);
+  // this.setState({ purchasable: sum > 0 });
   // }
 
   /**const objectMap = (obj, fn) =>
@@ -38,39 +37,15 @@ console.log(objectMap(myObject, v => 2 * v))  */
     }
   };
 
-  const storedAircraft1 = storedAircraft.map(d => (
-    <div key={d.tailNumber}>
-      <p>
-        Tail Number: <strong>{d.tailNumber}</strong>
-        <br />
-        Fuel Remaining (A/C Gauges): <strong>{d.initalFuel}Kg</strong> <br />
-        Specific Gravity: <strong>{d.specificGravity}</strong>
-        <br />
-        Fuel Put In (Kg): <strong>{d.deliveredFuelKg}Kg</strong>
-        <br />
-        Total A/C: <strong>{d.totalACFuelState}Kg</strong>
-        <br />
-        Kg Discrepancy: <strong>{d.discrepancy}%</strong>
-        <br />
-      </p>
-    </div>
-  ));
-
-  console.log(storedAircraft1)
-
   return (
     <div>
       <div className="input-cont">
         <div className="content-wrapper">
           <h1>Stored Aircraft Details</h1>
-          {/* {storedAircraft.map(([key, val], index) => (
-            <StoredAircraftLi
-              key={index}
-              className="input-box"
-              storedAircraft={storedAircraft}
-            />
-          ))} */}
-          <Paper>{storedAircraft1} </Paper>
+          <StoredAircraftLi
+            key={storedAircraft.tailNumber}
+            storedAircraft={storedAircraft}
+          />
         </div>
         <footer>
           <div className="button-wrapper">
--- src/components/StoredAircraft/StoredAircraftLi.js
+++ src/components/StoredAircraft/StoredAircraftLi.js
@@ -4,13 +4,31 @@ import Paper from '@material-ui/core/Paper';
 const StoredAircraftLi = props => {
   const [classEnabled, setClassEnabled] = useState(false);
 
+  const storedInfo = props.storedAircraft.map(d => (
+    <div key={d.tailNumber}>
+      <p>
+        Tail Number: <strong>{d.tailNumber}</strong>
+        <br />
+        Fuel Remaining (A/C Gauges): <strong>{d.initalFuel}Kg</strong> <br />
+        Specific Gravity: <strong>{d.specificGravity}</strong>
+        <br />
+        Fuel Put In (Kg): <strong>{d.deliveredFuelKg}Kg</strong>
+        <br />
+        Total A/C: <strong>{d.totalACFuelState}Kg</strong>
+        <br />
+        Kg Discrepancy: <strong>{d.discrepancy}%</strong>
+        <br />
+      </p>
+    </div>
+  ));
+
   return (
       <Paper
         className="input-box"
         onClick={() => setClassEnabled(!classEnabled)}
         style={classEnabled ? { textDecoration: "line-through" } : {}}
       >
-        {props.storedAircraft}
+        {storedInfo}
       </Paper>
   );
 };
