a58668b37b4fea4610c3b3ad3b2925c9bb27ccb6
Billy Needham <billyluisneedham@gmail.com>
2020-02-09 23:17:03 +0000

    Method to show stored aircraft

--- src/components/StoredAircraft/StoredAircraft.js
+++ src/components/StoredAircraft/StoredAircraft.js
@@ -1,9 +1,32 @@
 import React from "react";
 import StoredAircraftLi from "./StoredAircraftLi";
 import Button from "@material-ui/core/Button";
+import Paper from '@material-ui/core/Paper';
 
 const StoredAircraftCont = props => {
   const storedAircraft = props.storedAircraft;
+  
+
+    // Object.keys(ingredients)
+    //   .map(igKey => {
+    //     return ingredients[igKey];
+    //   })
+    //   .reduce((sum, el) => {
+    //     return sum + el;
+    //   }, 0);
+    // this.setState({ purchasable: sum > 0 });
+  // }
+
+  /**const objectMap = (obj, fn) =>
+  Object.fromEntries(
+    Object.entries(obj).map(
+      ([k, v], i) => [k, fn(v, k, i)]
+    )
+  )
+  
+const myObject = { a: 1, b: 2, c: 3 }
+
+console.log(objectMap(myObject, v => 2 * v))  */
 
   const confirmResetWindow = () => {
     if (
@@ -15,18 +38,34 @@ const StoredAircraftCont = props => {
     }
   };
 
+  const storedAircraft1 = storedAircraft.map(d => (
+    <div key={d.tailNumber}>
+      <p>
+        Tail Number: {d.tailNumber}
+        Fuel Remaining (A/C Gauges): {d.initalFuel}Kg 
+        Specific Gravity: {d.specificGravity}
+        Fuel Put In (Kg): {d.deliveredFuelKg}Kg
+        Total A/C: {d.totalACFuelState}Kg 
+        Discrepancy: {d.discrepancy}%
+      </p>
+    </div>
+  ));
+
+  console.log(storedAircraft1)
+
   return (
     <div>
       <div className="input-cont">
         <div className="content-wrapper">
           <h1>Stored Aircraft Details</h1>
-          {storedAircraft.map((storedAircraft, index) => (
+          {/* {storedAircraft.map(([key, val], index) => (
             <StoredAircraftLi
               key={index}
               className="input-box"
               storedAircraft={storedAircraft}
             />
-          ))}
+          ))} */}
+          <Paper>{storedAircraft1} </Paper>
         </div>
         <footer>
           <div className="button-wrapper">
